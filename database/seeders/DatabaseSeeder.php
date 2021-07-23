c72ea602a3311a7bf0fe7aa57b2ea3fbf390de5a
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2021-07-23 17:34:20 -0300

    Iniciando com Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,53 +16,66 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'John Doe',
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
+        Category::factory(3)->create();
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
+        for ($i = 0; $i <= 5; $i++) {
+            Post::factory(5)->create([
+                'user_id' => $user->id,
+                'category_id' => rand(1, 3),
+            ]);
+        }
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Personal Post',
-            'slug' => 'my-personal-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dignissimos dolor dolorem ex ipsum nam obcaecati omnis placeat quibusdam veniam! Ad, asperiores at consequuntur corporis earum explicabo nisi quia vel velit voluptatum. Ad asperiores consectetur consequuntur dignissimos neque pariatur tenetur, vero. Commodi culpa dolorum earum enim esse, in incidunt laudantium maxime modi nam perferendis recusandae rem rerum! Ad consequuntur ducimus itaque laudantium, perferendis quam temporibus. Aliquam, amet aut cupiditate debitis doloremque eaque iure laborum laudantium minima mollitia placeat praesentium quaerat quasi. Deleniti dolores dolorum eius eligendi ipsam iste itaque laborum necessitatibus odit praesentium quos repudiandae ullam voluptas voluptatem voluptates, voluptatibus.'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dignissimos dolor dolorem ex ipsum nam obcaecati omnis placeat quibusdam veniam! Ad, asperiores at consequuntur corporis earum explicabo nisi quia vel velit voluptatum. Ad asperiores consectetur consequuntur dignissimos neque pariatur tenetur, vero. Commodi culpa dolorum earum enim esse, in incidunt laudantium maxime modi nam perferendis recusandae rem rerum! Ad consequuntur ducimus itaque laudantium, perferendis quam temporibus. Aliquam, amet aut cupiditate debitis doloremque eaque iure laborum laudantium minima mollitia placeat praesentium quaerat quasi. Deleniti dolores dolorum eius eligendi ipsam iste itaque laborum necessitatibus odit praesentium quos repudiandae ullam voluptas voluptatem voluptates, voluptatibus.'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dignissimos dolor dolorem ex ipsum nam obcaecati omnis placeat quibusdam veniam! Ad, asperiores at consequuntur corporis earum explicabo nisi quia vel velit voluptatum. Ad asperiores consectetur consequuntur dignissimos neque pariatur tenetur, vero. Commodi culpa dolorum earum enim esse, in incidunt laudantium maxime modi nam perferendis recusandae rem rerum! Ad consequuntur ducimus itaque laudantium, perferendis quam temporibus. Aliquam, amet aut cupiditate debitis doloremque eaque iure laborum laudantium minima mollitia placeat praesentium quaerat quasi. Deleniti dolores dolorum eius eligendi ipsam iste itaque laborum necessitatibus odit praesentium quos repudiandae ullam voluptas voluptatem voluptates, voluptatibus.'
-        ]);
+//        User::truncate();
+//        Post::truncate();
+//        Category::truncate();
+//
+//        $user = User::factory()->create();
+//
+//        $personal = Category::create([
+//            'name' => 'Personal',
+//            'slug' => 'personal'
+//        ]);
+//
+//        $family = Category::create([
+//            'name' => 'Family',
+//            'slug' => 'family'
+//        ]);
+//
+//        $work = Category::create([
+//            'name' => 'Work',
+//            'slug' => 'work'
+//        ]);
+//
+//        Post::create([
+//            'user_id' => $user->id,
+//            'category_id' => $personal->id,
+//            'title' => 'My Personal Post',
+//            'slug' => 'my-personal-post',
+//            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+//            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dignissimos dolor dolorem ex ipsum nam obcaecati omnis placeat quibusdam veniam! Ad, asperiores at consequuntur corporis earum explicabo nisi quia vel velit voluptatum. Ad asperiores consectetur consequuntur dignissimos neque pariatur tenetur, vero. Commodi culpa dolorum earum enim esse, in incidunt laudantium maxime modi nam perferendis recusandae rem rerum! Ad consequuntur ducimus itaque laudantium, perferendis quam temporibus. Aliquam, amet aut cupiditate debitis doloremque eaque iure laborum laudantium minima mollitia placeat praesentium quaerat quasi. Deleniti dolores dolorum eius eligendi ipsam iste itaque laborum necessitatibus odit praesentium quos repudiandae ullam voluptas voluptatem voluptates, voluptatibus.'
+//        ]);
+//
+//        Post::create([
+//            'user_id' => $user->id,
+//            'category_id' => $work->id,
+//            'title' => 'My Work Post',
+//            'slug' => 'my-work-post',
+//            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+//            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dignissimos dolor dolorem ex ipsum nam obcaecati omnis placeat quibusdam veniam! Ad, asperiores at consequuntur corporis earum explicabo nisi quia vel velit voluptatum. Ad asperiores consectetur consequuntur dignissimos neque pariatur tenetur, vero. Commodi culpa dolorum earum enim esse, in incidunt laudantium maxime modi nam perferendis recusandae rem rerum! Ad consequuntur ducimus itaque laudantium, perferendis quam temporibus. Aliquam, amet aut cupiditate debitis doloremque eaque iure laborum laudantium minima mollitia placeat praesentium quaerat quasi. Deleniti dolores dolorum eius eligendi ipsam iste itaque laborum necessitatibus odit praesentium quos repudiandae ullam voluptas voluptatem voluptates, voluptatibus.'
+//        ]);
+//
+//        Post::create([
+//            'user_id' => $user->id,
+//            'category_id' => $family->id,
+//            'title' => 'My Family Post',
+//            'slug' => 'my-family-post',
+//            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+//            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dignissimos dolor dolorem ex ipsum nam obcaecati omnis placeat quibusdam veniam! Ad, asperiores at consequuntur corporis earum explicabo nisi quia vel velit voluptatum. Ad asperiores consectetur consequuntur dignissimos neque pariatur tenetur, vero. Commodi culpa dolorum earum enim esse, in incidunt laudantium maxime modi nam perferendis recusandae rem rerum! Ad consequuntur ducimus itaque laudantium, perferendis quam temporibus. Aliquam, amet aut cupiditate debitis doloremque eaque iure laborum laudantium minima mollitia placeat praesentium quaerat quasi. Deleniti dolores dolorum eius eligendi ipsam iste itaque laborum necessitatibus odit praesentium quos repudiandae ullam voluptas voluptatem voluptates, voluptatibus.'
+//        ]);
 
     }
 }
