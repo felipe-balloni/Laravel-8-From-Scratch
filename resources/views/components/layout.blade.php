1d70135b2d9a4c5427d0a586c6d3c8343f6d3008
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2021-08-04 13:55:46 -0300

    Ativação via formulário

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -33,25 +33,30 @@
             @endauth
 
             <a class="bg-blue-500 hover:bg-blue-700 ml-3 rounded-full text-xs font-semibold text-white uppercase px-5 py-3"
-               href="#">Subscribe for Updates</a>
+               href="#newsletter">Subscribe for Updates</a>
         </div>
+
+
+
     </nav>
 
     {{ $slot }}
 
-    <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+    <footer id="newsletter"
+            class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-10">
         <img alt="Lary Newsletter" class="mx-auto" src="/images/lary-newsletter-icon.svg">
         <h5 class="text-3xl">Stay in touch with the latest post</h5>
         <p class="text-sm">Promise to keep the inbox clean. No bugs. </p>
 
         <div class="mt-10">
             <div class="relative inline-block md:bg-gray-200 mx-auto rounded-full">
-                <form action="#" class="md:flex text-sm">
+                <form action="{{ route('subscribe') }}" method="POST" class="md:flex text-sm">
+                    @csrf
                     <div class="px-5 py-1 inline-flex bg-gray-200 mx-auto rounded-full">
                         <label class="hidden" for="mail">Your email address</label>
                         <img alt="mailbox letter" src="/images/mailbox-icon.svg">
                         <input class="focus:ring-0 focus:outline-none border-none bg-transparent pl-4" id="mail"
-                               name="mail"
+                               name="email"
                                placeholder="Your email address"
                                type="email">
                     </div>
@@ -64,7 +69,14 @@
         </div>
 
     </footer>
-
+    <div class="fixed z-30 bottom-0 right-0 mr-6 mb-6 bg-gray-400 opacity-50">
+        <a id="back2Top" href="#" class="hidden text-black bg-gray-800 bg-opacity-70 rounded-md">
+            <span class="hidden">Back to Top</span>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 24 24" class="h-10 w-10">
+                <path fill="currentColor" d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z" />
+            </svg>
+        </a>
+    </div>
 </section>
 
 <x-flash/>
